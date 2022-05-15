0b7308774427ed46fac168968a9cf8381d5faa67
Sergey Ippolitov <seripp28@gmail.com>
2022-05-15 23:32:46 +0300

    it can bounce out of contours

---
+++ ball.py
@@ -0,0 +1,129 @@
+import cv2
+import numpy as np
+from random import randint
+
+class Ball:
+    def __init__ (self,  x, y, r, clr):
+        #self.cnv = cnv
+        self.x = x
+        self.y = y
+        self.r = r
+        self.clr = clr
+        self.vx = randint(-20,20)
+        self.vy = randint(-20,20)
+
+        if self.vx > 0:
+            self.vecX = 1
+        else:
+            self.vecX = -1
+
+        if self.vy > 0:
+            self.vecY = 1
+        else:
+            self.vecY = -1
+            
+        #print(self.vx, self.vy)
+        self.back_clr = (1,1,1)
+        
+    def draw(self, cnv):
+        cv2.circle( cnv, (self.x,self.y), self.r, self.clr, -1 )
+        cv2.circle( cnv, ( self.x + self.vx + self.r *  self.vecX,
+                                    self.y + self.vy + self.r *  self.vecY ),
+                    3, (0,0,255), -1 )
+        
+    def destroy(self,cnv):
+        cv2.circle( cnv, (self.x,self.y), self.r, self.back_clr, -1 )
+
+    def bounceX (self):
+        self.vx *= -1
+        self.vecX *= -1
+    def bounceY(self):
+        self.vy *= -1
+        self.vecY *= -1
+
+    def collisionContourCheck(self, conts):
+       
+             
+        try:
+                if cv2.pointPolygonTest(conts[0], # контур
+                                                              ( self.x + self.vx + self.r *  self.vecX, #  следующий шаг  X
+                                                              self.y + self.vy + self.r *  self.vecY ), #                                Y
+                                                         True) >= 0: # если в контуре, то происходит отскок                  
+                #if randint(0,1):
+                    self.bounceX ()
+                #else:
+                    self.bounceY ()
+                    if abs(self.vx) < 50:
+                       self.vx  = int(self.vx*2)
+                    if abs(self.vy) < 50:
+                       self.vy  = int(self.vy*2)
+        except:
+            pass
+
+        
+    def move(self,cnv, conts=[] ):
+        
+        self.destroy(cnv)
+
+        # X-es
+
+        # right border
+        if self.x + self.vx + self.r  > cnv.shape[1]:
+            self.bounceX()
+            
+            if abs(self.vx) > 2:
+               self.vx  = int(self.vx*0.9)
+               
+        # left border
+        if self.x + self.vx - self.r  < 0:
+            self.bounceX()
+            if abs(self.vx) > 2:
+               self.vx  = int(self.vx*0.9)
+
+        # Y-es
+        
+        #  down border
+        if self.y + self.vy + self.r  > cnv.shape[0]:
+            self.bounceY()
+            if abs(self.vy) > 2:
+               self.vy  = int(self.vy*0.9)
+        # upper border
+        if self.y + self.vy - self.r  < 0:
+            self.bounceY()
+            if abs(self.vy) > 2:
+               self.vy  = int(self.vy*0.9)
+
+        self.collisionContourCheck(conts)
+        # Dx Dy
+        self.x += self.vx
+        self.y += self.vy
+
+        self.draw(cnv)
+        
+    def giveVelocity(self, vx, vy):
+        self.vx += vx
+        self.vy += vy
+        
+if __name__ == '__main__':
+    cnv = np.ones( (600, 600, 3), dtype=np.uint8() )
+
+    ball = Ball( 300+ randint(0,100), 300+ randint(0,100), 50, (0,255,255))
+
+    while True:
+
+        ball.move(cnv)
+        cv2.imshow('main', cnv)
+            
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+        if key == ord('w'):
+            ball.giveVelocity(2, 0)
+        if key == ord('s'):
+            ball.giveVelocity(-2, 0)
+        if key == ord('a'):
+            ball.giveVelocity(0, -2)
+        if key == ord('d'):
+            ball.giveVelocity(0, 2)
+    
+    cv2.destroyAllWindows()
---
+++ pongAR.py
@@ -0,0 +1,81 @@
+import cv2
+from random import randint
+import ball
+import igrok
+
+
+cap = cv2.VideoCapture(0)
+
+cap.set(3,1920) # Установление длины окна
+cap.set(4,1080)  # Ширина окна
+
+
+
+mrB = ball.Ball(320+ randint(0,100), 240+ randint(0,100), 50, (0,255,255))
+
+
+def findContour (mask, out):
+    cont, h = cv2.findContours( mask, cv2.RETR_TREE,
+                                cv2.CHAIN_APPROX_SIMPLE)
+    cont = sorted( cont,key=cv2.contourArea, reverse=True)
+    cv2.drawContours( out, cont, 0, (0,255,0), 2 )
+   
+    
+    try:
+        for count, i in enumerate(cont[0]):
+        #print(i[0][0])
+            
+            if count % 1 == 0:
+                cv2.line(out, (i[0][0], i[0][1]),
+                          (cont[0][len(cont[0]) - count - 1][0][0],
+                           cont[0][len(cont[0]) - count - 1][0][1]),
+                    (randint(0,255), randint(0,255), randint(0,255)), 2)
+            
+            out[i[0][1], i[0][0], 1] = 255
+        return cont[0]
+    except:
+       return []
+       
+    
+
+    
+while True:
+    
+    tr, frame = cap.read()
+    frame = cv2.flip(frame, 2)
+    frame_ = cv2.blur( frame, (30, 30) )
+    frame_HSV = cv2.cvtColor( frame_,
+                              cv2.COLOR_BGR2HSV )
+
+    clr_low = ( 0, 100 ,80)
+    clr_high = (15, 255, 255)
+
+    frame_clr = cv2.inRange( frame_HSV,clr_low,clr_high)
+    conts = findContour (frame_clr, frame)
+
+    
+  
+
+        
+    mrB.move(frame, [conts])
+    
+    
+
+    
+    cv2.imshow('mask', frame_clr)    
+    cv2.imshow('main', frame )
+    key = cv2.waitKey(1)
+    
+    if key == 27:
+        break
+    if key == ord('w'):
+        mrB.giveVelocity(2, 0)
+    if key == ord('s'):
+        mrB.giveVelocity(-2, 0)
+    if key == ord('a'):
+        mrB.giveVelocity(0, -2)
+    if key == ord('d'):
+        mrB.giveVelocity(0, 2)
+
+cv2.destroyAllWindows()
+cap.release()
